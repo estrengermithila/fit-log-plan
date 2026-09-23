29b44512d7e1b41de8c50d1c58a8bf6b0f797174
estrengermithila <mithilafarjanam20@gmail.com>
2026-09-23 23:46:36 +0600

    set Navbar Properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-       <div>
+       <div className="w-full max-w-[1440px] mx-auto">
         <Navbar/>
          {children}
          <Footer></Footer>
---
+++ src/app/workOut/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkOut = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WorkOut;
